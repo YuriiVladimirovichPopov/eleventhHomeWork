8e515a36c94f749d0da323cd30a5463f135306fb
YuriiVladimirovichPopov <papanchik87@yahoo.com>
2023-11-28 19:13:35 +0500

    fix 1

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { app } from './settings';
 import { runDb } from './db/db';
 
 
-const port = process.env.PORT || 3033
+const port = process.env.PORT || 3034
 
 const startingApp = async () => {
   await runDb()
