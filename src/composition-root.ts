02be734a4e7f33b6afd4d0aed80696f756976f24
YuriiVladimirovichPopov <papanchik87@yahoo.com>
2023-12-21 21:56:02 +0500

    fix blogsController

--- src/composition-root.ts
+++ src/composition-root.ts
@@ -1,14 +1,19 @@
 import { AuthService } from "./application/auth-service";
+import { BlogService } from "./application/blog-service";
+import { PostsService } from "./application/post-service";
 import { AuthController } from "./controllers/authController";
+import { BlogsController } from "./controllers/blogsController";
 import { SecurityController } from "./controllers/securityController";
 import { UserController } from "./controllers/userController";
+import { QueryPostRepository } from "./query repozitory/queryPostsRepository";
 import { QueryUserRepository } from "./query repozitory/queryUserRepository";
 import { DeviceRepository } from "./repositories/device-repository";
 import { UsersRepository } from "./repositories/users-repository";
 
 
-
-
+const blogService = new BlogService()
+const postsService = new PostsService()
+const queryPostRepository = new QueryPostRepository()
 const usersRepository = new UsersRepository()
 const queryUserRepository = new QueryUserRepository()
 const deviceRepository = new DeviceRepository()
@@ -31,4 +36,10 @@ export const securityController = new SecurityController(
     queryUserRepository,
     authService,
     deviceRepository
+)
+
+export const blogsController = new BlogsController(
+    blogService,
+    postsService,
+    queryPostRepository
 )
--- src/routers/blogs-router.ts
+++ src/routers/blogs-router.ts
@@ -1,6 +1,4 @@
-import { Request, Response, Router } from "express";
-import { BlogService } from "../application/blog-service";
-import { httpStatuses } from "./helpers/send-status";
+import { Router } from "express";
 import {
   authorizationValidation,
   inputValidationErrors,
@@ -10,114 +8,10 @@ import {
   updateBlogValidation,
 } from "../middlewares/validations/blogs.validation";
 import { createPostValidationForBlogRouter } from "../middlewares/validations/posts.validation";
-import { RequestWithParams, RequestWithBody } from "../types";
-import { BlogInputModel } from "../models/blogs/blogsInputModel";
-import { getByIdParam } from "../models/getById";
-import { BlogViewModel } from "../models/blogs/blogsViewModel";
-import { QueryPostRepository } from "../query repozitory/queryPostsRepository";
-import { getPaginationFromQuery, PaginatedType } from "./helpers/pagination";
-import { Paginated } from "./helpers/pagination";
-import { PostsInputModel } from "../models/posts/postsInputModel";
-import { PostsService } from "../application/post-service";
-import { PostsViewModel } from "../models/posts/postsViewModel";
+import { blogsController } from "../composition-root";
 
 export const blogsRouter = Router({});
 
-class BlogsController {
-  private blogService: BlogService;
-  private postsService: PostsService;
-  private queryPostRepository: QueryPostRepository;
-  constructor() {
-    this.blogService = new BlogService();
-    this.postsService = new PostsService();
-    this.queryPostRepository = new QueryPostRepository();
-  }
-
-  async getAllBlogs(req: Request, res: Response) {
-    const pagination = getPaginationFromQuery(
-      req.query as unknown as PaginatedType,
-    );
-    const allBlogs: Paginated<BlogViewModel> =
-      await this.blogService.findAllBlogs(pagination);
-
-    return res.status(httpStatuses.OK_200).send(allBlogs);
-  }
-  async createBlogs(
-    req: RequestWithBody<BlogViewModel>,
-    res: Response<BlogViewModel>,
-  ) {
-    const newBlog = await this.blogService.createBlog(req.body);
-    return res.status(httpStatuses.CREATED_201).send(newBlog);
-  }
-  async getPostByBlogId(
-    req: Request<{ blogId: string }, {}, {}, {}>,
-    res: Response,
-  ) {
-    const blogWithPosts = await this.blogService.findBlogById(
-      req.params.blogId,
-    );
-    if (!blogWithPosts) {
-      return res.sendStatus(httpStatuses.NOT_FOUND_404);
-    }
-    const pagination = getPaginationFromQuery(
-      req.query as unknown as PaginatedType,
-    );
-    const foundBlogWithAllPosts: Paginated<PostsViewModel> =
-      await this.queryPostRepository.findAllPostsByBlogId(
-        req.params.blogId,
-        pagination,
-      );
-
-    return res.status(httpStatuses.OK_200).send(foundBlogWithAllPosts);
-  }
-  async createPostForBlogById(req: Request, res: Response) {
-    const blogId = req.params.blogId;
-
-    const { title, shortDescription, content } = req.body;
-
-    const newPostForBlogById: PostsInputModel | null =
-      await this.postsService.createPost({
-        title,
-        shortDescription,
-        content,
-        blogId,
-      });
-
-    if (newPostForBlogById) {
-      return res.status(httpStatuses.CREATED_201).send(newPostForBlogById);
-    }
-    return res.sendStatus(httpStatuses.NOT_FOUND_404);
-  }
-  async getBlogById(
-    req: RequestWithParams<getByIdParam>,
-    res: Response<BlogViewModel>,
-  ) {
-    const foundBlog = await this.blogService.findBlogById(req.params.id);
-    if (!foundBlog) return res.sendStatus(httpStatuses.NOT_FOUND_404);
-
-    return res.status(httpStatuses.OK_200).send(foundBlog);
-  }
-  async updateBlogById(
-    req: Request<getByIdParam, BlogInputModel>,
-    res: Response<BlogViewModel>,
-  ) {
-    const updateBlog = await this.blogService.updateBlog(
-      req.params.id,
-      req.body,
-    );
-    if (!updateBlog) return res.sendStatus(httpStatuses.NOT_FOUND_404);
-
-    return res.sendStatus(httpStatuses.NO_CONTENT_204);
-  }
-  async deleteBlogById(req: RequestWithParams<getByIdParam>, res: Response) {
-    const foundBlog = await this.blogService.deleteBlog(req.params.id);
-    if (!foundBlog) {
-      return res.sendStatus(httpStatuses.NOT_FOUND_404);
-    }
-    return res.sendStatus(httpStatuses.NO_CONTENT_204);
-  }
-}
-const blogsController = new BlogsController();
 
 blogsRouter.get("/", blogsController.getAllBlogs.bind(blogsController));
 blogsRouter.post(
